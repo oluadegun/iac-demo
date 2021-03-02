1a93a5af1506f08fff445b5795249827446d82bb
Olu Adegun <olu.adegun@maplequad.com>
2021-03-02 16:05:33 +0000

    testing

--- lambda.tf
+++ lambda.tf
@@ -19,11 +19,11 @@ EOF
 }
 
 variable "file_name" {
-    default = "${path.module}/files/hello_world.zip"
+    default = null
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename      = var.file_name
+  filename      = var.filename == null ? "${path.module}/files/hello_world.zip" : var.filename
   function_name = "trading_point_demo"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
