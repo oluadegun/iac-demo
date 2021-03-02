187898c4789f6422a525023d15e373d8ec2344d9
Olu Adegun <olu.adegun@maplequad.com>
2021-03-02 11:10:28 +0000

    removed bucket

--- backend.tf
+++ backend.tf
@@ -3,22 +3,22 @@ provider "aws" {
 }
 
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tf-state-1993"
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "tf-state-1993"
+#   # Enable versioning so we can see the full revision history of our
+#   # state files
+#   versioning {
+#     enabled = true
+#   }
+#   # Enable server-side encryption by default
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "tf-state-1993-locks"
